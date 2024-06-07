b10e3505cc4af79033add185889dbb610215a601
Meriem Helali <helalimeriem.mi@gmail.com>
2024-06-07 12:17:38 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Navbar, Container, Nav, Card, Row, Col } from "react-bootstrap";
 
 function App() {
+  // <React.Fragment> or <> because jsx return only one element
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <div className="App">
+        <Navbar bg="dark" variant="dark">
+          <Container>
+            <Nav className="me-auto">
+              <Nav.Link href="#link1">Link 01</Nav.Link>
+              <Nav.Link href="#link2">Link 02</Nav.Link>
+              <Nav.Link href="#link3">Link 03</Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
+        <Container className="mt-4">
+          <h1 className="text-center">CheckPoint 02 : React js</h1>
+          <Row className="mt-4">
+            <Col>
+              <Card>
+                <Card.Body>
+                  <Card.Title>Card Title 1</Card.Title>
+                  <Card.Text>this is the first card text</Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col>
+              <Card>
+                <Card.Body>
+                  <Card.Title>Card Title 2</Card.Title>
+                  <Card.Text>this is the second card text</Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col>
+              <Card>
+                <Card.Body>
+                  <Card.Title>Card Title 3</Card.Title>
+                  <Card.Text>this is the third card text</Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </React.Fragment>
   );
 }
 
